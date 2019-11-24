dc698b4763f71d5b6d88c7b52c15309d9d9a6dc5
dormantbs <714810490@qq.com>
2019-11-24 19:15:40 +0800

    Some correts about hw6

--- hw6/sources/main.cpp
+++ hw6/sources/main.cpp
@@ -1,9 +1,19 @@
 #include <algorithm>
 #include <random>
 #include <iostream>
+#include "customset.h"
+#include "lotterystation.h"
+#include "tickets.h"
 #include <cmath>
 
 int main() {
-	
+	int a[6] = { 1,2,3,4,5,6 };
+	CustomSet<int> foo(a, 6);
+	LotteryStation sample;
+	sample.newRound();
+	sample.setPrizeNumbers(foo, LOTTO6); // LOTTO6 is predefined
+	LotteryTicket* jackpot = sample.buy(foo, LOTTO6);
+	// prints "Bought a Lotto 6 ticket for 2 Yuan at round 1.\n"
+	sample.claimPrize(jackpot);
 }
 
--- hw6/sources/tickets.cpp
+++ hw6/sources/tickets.cpp
@@ -1,9 +1,30 @@
 #include "tickets.h"
 
+inline int get_money(int num, int tickettype) {
+	if (tickettype == LOTTO6) ++num;
+
+	switch (num) {
+		case 7: return 2000000; break;
+		case 6: return 4500; break;
+		case 5: return  75; break;
+		case 4: return 5; break;
+		default: return 0; break;
+	}
+}
+
+inline int C(int n, int m) {
+	int up = 1, down = 1;
+	for (int i = 1; i <= m; ++i) {
+		up = up * (n - i + 1);
+		down = down * i;
+	}
+	return up / down;
+}
+
 LotteryTicket::LotteryTicket(const CustomSet<int>& numbers, int round) {
 	m_numbers += numbers;
-	m_numbers.printSet();
 	m_round = round;
+	m_cost = -1;
 	claimed = false;
 }
 
@@ -31,13 +52,8 @@ bool LotteryTicket::isClaimed() const{
 Lotto7Ticket::
 Lotto7Ticket(const CustomSet<int>& numbers, int round) :
 	LotteryTicket(numbers, round) {
-	int down = 1, up = 1, siz = numbers.size();
-	for (int i = 1; i <= LOTTO7; ++i) {
-		up = up * (siz - i + 1);
-		down = down * i;
-	}
-
-	this->setCost((up << 1) / down);
+	int siz = numbers.size();
+	this->setCost(C(siz, LOTTO7) << 1);
 }
 
 int Lotto7Ticket::getTicketType() {
@@ -46,39 +62,23 @@ int Lotto7Ticket::getTicketType() {
 
 int Lotto7Ticket::claimPrize(const CustomSet<int>& prizeNumbers, int round) {
 	CustomSet<int> res = m_numbers.intersection(prizeNumbers);
-	int num = res.size();
+	int num = res.size(), ret = m_numbers.size() - num;
 	if (round != this->getRound() || this->isClaimed()) return -1;
 
 	this->setClaimed();
-	switch (num) {
-	case 7:
-		return 2000000;
-		break;
-	case 6:
-		return 4500;
-		break;
-	case 5:
-		return  75;
-		break;
-	case 4:
-		return 5;
-		break;
-	default:
-		return 0;
-		break;
+	int ans = 0;
+	for (int i = num, j = LOTTO7 - num; i >= 4 && j <= ret; --i, ++j) {
+		ans = ans + C(num, i) * C(ret, j) * get_money(i, LOTTO7);
 	}
+
+	return ans;
 }
 
 Lotto6Ticket::
 Lotto6Ticket(const CustomSet<int>& numbers, int round) :
 	LotteryTicket(numbers, round) {
-	int down = 1, up = 1, siz = numbers.size();
-	for (int i = 1; i <= LOTTO6; ++i) {
-		up = up * (siz - i + 1);
-		down = down * i;
-	}
-
-	this->setCost((up << 1) / down);
+	int siz = numbers.size();
+	this->setCost(C(siz, LOTTO6) << 1);
 }
 
 int Lotto6Ticket::getTicketType() {
@@ -87,25 +87,14 @@ int Lotto6Ticket::getTicketType() {
 
 int Lotto6Ticket::claimPrize(const CustomSet<int>& prizeNumbers, int round) {
 	CustomSet<int> res = m_numbers.intersection(prizeNumbers);
-	int num = res.size();
+	int num = res.size(), ret = m_numbers.size() - num;
 	if (round != this->getRound() || this->isClaimed()) return -1;
 
 	this->setClaimed();
-	switch (num) {
-	case 6:
-		return 2000000;
-		break;
-	case 5:
-		return 4500;
-		break;
-	case 4:
-		return  75;
-		break;
-	case 3:
-		return 5;
-		break;
-	default:
-		return 0;
-		break;
+	int ans = 0;
+	for (int i = num, j = LOTTO6 - num; i >= 3 && j <= ret; --i, ++j) {
+		ans = ans + C(num, i) * C(ret, j) * get_money(i, LOTTO6);
 	}
+
+	return ans;
 }
